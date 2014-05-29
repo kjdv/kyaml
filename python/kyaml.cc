2bc14ad51776074b6a58d0a9f9c4df661b323fbc
Klaas de Vries <klaas@klaasgaaf.nl>
2014-05-29 19:33:21 +0200

    dummies

--- python/kyaml.cc
+++ python/kyaml.cc
@@ -1,2 +1,21 @@
-#include "Python.h"
+#include <Python.h>
 #include "kyaml.hh"
+
+namespace
+{
+  PyObject *dummy(PyObject *self, PyObject *arg)
+  {
+    return NULL;
+  }
+
+  PyMethodDef module_methods[] =
+  {
+    {"dummy", (PyCFunction)dummy, METH_NOARGS, ""},
+    {NULL, NULL, 0, NULL}
+  };
+}
+
+PyMODINIT_FUNC initpykyaml()
+{
+  Py_InitModule3("kyaml", module_methods, "");
+}
